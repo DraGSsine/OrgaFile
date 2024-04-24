4d41bbc21dbf1cd2c4916f00b902101ba8604c27
ybenlafk <yben0600@gmail.com>
2024-04-24 14:11:43 +0100

    start working on settings page and user logic in the server side

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,12 +6,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { UploadModule } from './upload/upload.module';
+import { UserModule } from './user/user.module';
+
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: '.env',
     isGlobal: true,
-  }),AuthModule,MongooseModule.forRoot("mongodb+srv://dragssine:Jvunu5pgBA2FsBou@fleeso.j7mpc5c.mongodb.net/?retryWrites=true&w=majority&appName=fleeso/"), DashboardModule, UploadModule],
+  }),AuthModule,MongooseModule.forRoot("mongodb+srv://dragssine:Jvunu5pgBA2FsBou@fleeso.j7mpc5c.mongodb.net/?retryWrites=true&w=majority&appName=fleeso/"), DashboardModule, UploadModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ frontend/src/components/dashboard/settings/signupPage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import SignupPageForm from "./SignupPageForm";
+import Link from "next/link";
+
+const SignupPage = () => {
+  return (
+    <div className="w-full md:w-[50%] lg:w-[60%] flex flex-col items-center justify-center">
+      <div className="w-[80%] py-20">
+        <div>
+          <h1 className=" font-semibold text-3xl pb-3">Sign up</h1>
+          <p className=" pb-7">Have an account <Link href='/auth/signin' className=" font-semibold text-primary-color">sign in</Link> </p>
+        </div>
+        <SignupPageForm />
+      </div>
+    </div>
+  );
+};
+
+export default SignupPage;
