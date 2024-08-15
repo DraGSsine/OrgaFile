8c26947b6fd96d18e5d4c31e6b0b147720a92595
Yassine Ouchen <youchen@e3r1p1.1337.ma>
2024-08-15 16:47:00 +0100

    fix

--- frontend/src/app/payment-successful/page.tsx
+++ frontend/src/app/payment-successful/page.tsx
@@ -1,9 +1,3 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/51dLYgBmfgR
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
-
 import { Button } from "@nextui-org/button";
 import Link from "next/link";
 
