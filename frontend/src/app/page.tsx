a894ac17db402a0d704d5439ed4f79131a2f61fa
Yassine Ouchen <youchen@e3r1p3.1337.ma>
2024-09-26 12:29:12 +0100

    upload

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,24 +3,9 @@ import Features from "@/components/features/Features";
 import FooterBar from "@/components/footerBar/FooterBar";
 import Hero from "@/components/home/Hero";
 import HowItWorks from "@/components/howItWorks/HowItWork";
-import NavBar from "@/components/NavBar";
 import NewsLetter from "@/components/newsLetter/NewsLetter";
-import axios from "axios";
-import Image from "next/image";
-import { useEffect } from "react";
 
 export default function Home() {
-  const fetchTest = async () => {
-    try {
-      const data = await axios.post("https://doctify-m1igesmgd-dragssines-projects.vercel.app/api/tests",{})
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    fetchTest();
-  }), [];
   return (
     <main>
       <Hero />
