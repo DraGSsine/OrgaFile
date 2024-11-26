db613c6d675443110b5882ed38a8ac280ef52551
Yassine Ouchen <youchen@e3r1p1.1337.ma>
2024-11-26 19:29:59 +0100

    update the sameSite to none

--- backend/src/helpers/constant.ts
+++ backend/src/helpers/constant.ts
@@ -3,7 +3,7 @@ import { CookieOptions } from 'express';
 export const resHeaders: CookieOptions = {
   httpOnly: true,
   secure: true,
-  sameSite: 'lax',
+  sameSite: 'none',
   maxAge: 1000 * 60 * 60 * 24 * 7,
   domain: 'orgafile.com',
   path: '/',
