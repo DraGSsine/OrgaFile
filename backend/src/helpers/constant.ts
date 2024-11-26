8efdb9303bda6d0f30629f1d0d047c9a43ba929d
Yassine Ouchen <youchen@e3r1p1.1337.ma>
2024-11-26 19:16:50 +0100

    change the resHeaser to sercure=true

--- backend/src/helpers/constant.ts
+++ backend/src/helpers/constant.ts
@@ -2,7 +2,7 @@ import { CookieOptions } from 'express';
 
 export const resHeaders: CookieOptions = {
   httpOnly: true,
-  secure: false,
+  secure: true,
   sameSite: 'lax',
   maxAge: 1000 * 60 * 60 * 24 * 7,
 };
