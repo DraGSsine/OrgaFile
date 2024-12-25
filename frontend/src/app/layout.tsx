00dac7b3abe7ad3dd4bfee0c31ed61cbf10f61f9
Yassine Ouchen <youchen@e3r1p1.1337.ma>
2024-12-25 06:39:29 +0100

    add analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/providers/Provider";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({
   weight: ["100", "200", "300", "400", "500", "600"],
   subsets: ["vietnamese"],
