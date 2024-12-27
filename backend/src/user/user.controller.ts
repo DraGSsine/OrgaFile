62ff9e1220aab4880fc2ba61b5a30ed509303f28
Yassine Ouchen <youchen@e3r1p1.1337.ma>
2024-12-27 15:04:57 +0100

    Make the user logout when he delete his account

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -7,37 +7,49 @@ import {
   UseGuards,
   Get,
   Res,
-} from '@nestjs/common';
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { AuthGuard } from '../guards/auth.guard';
-import { UpdatePasswordDto } from './dto/update-password.dto';
+} from "@nestjs/common";
+import { UserService } from "./user.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { AuthGuard } from "../guards/auth.guard";
+import { UpdatePasswordDto } from "./dto/update-password.dto";
+import { CookieOptions } from "express";
 
 @UseGuards(AuthGuard)
-@Controller('api/user')
+@Controller("api/user")
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  private resHeaders: CookieOptions = {
+    domain: process.env.PROD === "true" ? ".orgafile.com" : "localhost",
+    sameSite: process.env.PROD === "true" ? "none" : "lax",
+    secure: process.env.PROD === "true" ? true : false,
+    httpOnly: true,
+  };
   @Get()
   getUserInfo(@Req() req: any) {
     return this.userService.getUserInfo(req.user.userId);
   }
   @Put("update-profile")
   create(@Body() createUserDto: CreateUserDto, @Req() req: any) {
-    
-    return this.userService.updateProfile(createUserDto,req.user.userId);
+    return this.userService.updateProfile(createUserDto, req.user.userId);
   }
 
   @Put("update-password")
-  updatePassword(@Body() updatePassowrdDto:UpdatePasswordDto , @Req() req: any) {
-    console.log(updatePassowrdDto)
+  updatePassword(
+    @Body() updatePassowrdDto: UpdatePasswordDto,
+    @Req() req: any
+  ) {
+    console.log(updatePassowrdDto);
     return this.userService.updatePassword(updatePassowrdDto, req.user.userId);
   }
 
-  @Delete('delete')
+  @Delete("delete")
   async remove(@Req() req: any, @Res() res: any) {
     await this.userService.remove(req.user.userId);
-    res.clearCookie('token');
-    return res.send({ message: 'user removed' });
+    const cookies = req.cookies;
+    for (const cookie in cookies) {
+      res.clearCookie(cookie, this.resHeaders);
+    }
+    return res.send({ message: "user removed" });
   }
 }
