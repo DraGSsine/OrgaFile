1a8f57f03b3e1b36e90d5691ae1de3851b9d18de
Yassine Ouchen <youchen@e3r1p1.1337.ma>
2025-01-01 06:43:50 +0100

    fix some cors problems

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import cookieParser = require('cookie-parser');
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import cookieParser = require("cookie-parser");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
   app.enableCors({
-    origin: process.env.PROD === 'true' ? 'https://orgafile.com' : 'http://localhost:3000',
+    origin:
+      process.env.PROD === "true"
+        ? "https://orgafile.com"
+        : "http://localhost:3000",
     credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Accept, Authorization",
   });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
