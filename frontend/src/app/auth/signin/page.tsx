c5d30b6920dc434540c9d753c1eff5893b833ea4
dragssine <ouchen606@gmail.com>
2025-01-13 16:22:58 +0100

    fix some typo

--- frontend/src/app/auth/signin/page.tsx
+++ frontend/src/app/auth/signin/page.tsx
@@ -12,10 +12,10 @@ export default function SignInPage() {
     <div className="w-full md:w-[60%] flex flex-col items-center justify-center bg-gradient-to-br from-background via-background/98 to-background/95">
       <div className="w-[85%] max-w-[480px] py-16 relative">
         <AuthWelcome
-          title="Create your account"
-          paragraph="Already have an account?"
-          authLink="/auth/signin"
-          authTitle="Sign in"
+          title="Sign in to OrgaFile"
+          paragraph="Don't have an account?"
+          authLink="/auth/signup"
+          authTitle="Sign up"
         />
         <div className="relative mb-8">
           <div className="absolute inset-0 flex items-center">
--- frontend/src/app/auth/signup/page.tsx
+++ frontend/src/app/auth/signup/page.tsx
@@ -31,7 +31,7 @@ export default function SignUpPage() {
           </div>
         </div>
 
-        <SignInForm />
+        <SignupPageForm />
       </div>
     </div>
   );
