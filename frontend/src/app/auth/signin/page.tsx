f4b2aedb6f8227bf1fe49888fb3200c30f70d570
dragssine <ouchen606@gmail.com>
2025-01-13 06:12:11 +0100

    fix some build errors

--- frontend/src/app/auth/signin/page.tsx
+++ frontend/src/app/auth/signin/page.tsx
@@ -1,35 +1,34 @@
+import { AuthWelcome } from "@/components/signup/AuthWelcome";
+import { Metadata } from "next";
+import SignInForm from "@/components/signIn/signInForm";
 
-import SignInForm from '@/components/signIn/signInForm'
-import { AuthWelcome } from '@/components/signup/AuthWelcome'
-import { Metadata } from 'next'
-import React from 'react'
+export const metadata: Metadata = {
+  title: "Sign In",
+  description: "Sign in to your account and get started with OrgaFile.",
+};
 
-export const metadata:Metadata = {
-  title: 'Sign Up',
-  description: "Sign up for an account and get started with OrgaFile.",
-}
-
-export const Page = () => {
+export default function SignInPage() {
   return (
-    <div className="w-full md:w-[60%] lg:w-[60%] flex flex-col items-center justify-center bg-gradient-to-br from-background via-background/98 to-background/95">
+    <div className="w-full md:w-[60%] flex flex-col items-center justify-center bg-gradient-to-br from-background via-background/98 to-background/95">
       <div className="w-[85%] max-w-[480px] py-16 relative">
-      <AuthWelcome title="Sign in to your account" paragraph="Don't have an account?" authLink="/auth/signup" authTitle="Sign up" />
+        <AuthWelcome
+          title="Create your account"
+          paragraph="Already have an account?"
+          authLink="/auth/signin"
+          authTitle="Sign in"
+        />
         <div className="relative mb-8">
           <div className="absolute inset-0 flex items-center">
             <div className="w-full border-t border-border" />
           </div>
           <div className="relative flex justify-center text-xs uppercase">
-            <span className=" bg-background px-2 text-muted-foreground">
+            <span className="bg-background px-2 text-muted-foreground">
               Or continue with email
             </span>
           </div>
         </div>
-
         <SignInForm />
       </div>
     </div>
   );
-};
-
-
-export default Page
+}
--- frontend/src/app/auth/signup/page.tsx
+++ frontend/src/app/auth/signup/page.tsx
@@ -1,19 +1,25 @@
-import SignupPageForm from '@/components/signup/SignupPageForm'
-import Discover from '@/components/signup/discover'
-import { AuthWelcome } from '@/components/signup/AuthWelcome'
-import { Metadata } from 'next'
-import React from 'react'
+import SignupPageForm from "@/components/signup/SignupPageForm";
+import Discover from "@/components/signup/discover";
+import { AuthWelcome } from "@/components/signup/AuthWelcome";
+import { Metadata } from "next";
+import React from "react";
+import SignInForm from "@/components/signIn/signInForm";
 
-export const metadata:Metadata = {
-  title: 'Sign In',
-  description: "Sign in to your account and get started with OrgaFile.",
-}
+export const metadata: Metadata = {
+  title: "Sign Up",
+  description: "Sign up to your account and get started with OrgaFile.",
+};
 
-const Page = () => {
+export default function SignUpPage() {
   return (
     <div className=" w-full md:w-[60%] flex flex-col items-center justify-center bg-gradient-to-br from-background via-background/98 to-background/95">
       <div className="w-[85%] max-w-[480px] py-16 relative">
-        <AuthWelcome title="Create your account" paragraph="Already have an account?" authLink="/auth/signin" authTitle="Sign in" />
+        <AuthWelcome
+          title="Create your account"
+          paragraph="Already have an account?"
+          authLink="/auth/signin"
+          authTitle="Sign in"
+        />
         <div className="relative mb-8">
           <div className="absolute inset-0 flex items-center">
             <div className="w-full border-t border-border" />
@@ -25,10 +31,8 @@ const Page = () => {
           </div>
         </div>
 
-        <SignupPageForm />
+        <SignInForm />
       </div>
     </div>
   );
-}
-
-export default Page
+};
